c41efb802488ad68bcd6289b7fedc18cdd000b72
HadisehIzadi <110168673+HadisehIzadi@users.noreply.github.com>
2024-01-31 11:52:20 +0330

    create project

---
+++ Paint Hit/Assets/Scripts/Color Scripts/ColorScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorScript : MonoBehaviour {
+
+    public Color[] color1;
+    public Color[] color2;
+    public Color[] color3;
+
+    public static Color[] colorArray;
+
+    void OnEnable ()
+    {
+        ChangeColor();
+	}
+
+    void ChangeColor()
+    {
+        int randomC = Random.Range(0, 2);
+
+        PlayerPrefs.SetInt("ColorSelect", randomC);
+        PlayerPrefs.GetInt("ColorSelect");
+
+        if (PlayerPrefs.GetInt("ColorSelect") == 0)
+            colorArray = color1;
+
+        if (PlayerPrefs.GetInt("ColorSelect") == 1)
+            colorArray = color2;
+
+        if (PlayerPrefs.GetInt("ColorSelect") == 2)
+            colorArray = color3;
+    }
+
+	
+}
